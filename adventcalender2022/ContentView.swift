2e4f1bc11926d1b3ee717200bb53050512093659
Shota-Kobayashi-RE <kobayashi_shota@retail-ai.jp>
2022-12-12 14:47:40 +0900

    Initial Commit

---
+++ adventcalender2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  adventcalender2022
+//
+//  Created by 小林　将太 on 2022/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ adventcalender2022/adventcalender2022App.swift
@@ -0,0 +1,17 @@
+//
+//  adventcalender2022App.swift
+//  adventcalender2022
+//
+//  Created by 小林　将太 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct adventcalender2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
